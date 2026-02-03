e786176eff0422daa03a02f28173cd8930cf7229
hannahwn <hannahwanjikunyongo@gmail.com>
2026-02-03 22:21:22 +0100

    added transaction objects

--- finance-tracker/data.js
+++ finance-tracker/data.js
@@ -1,2 +1,53 @@
 // Place here the transaction data array. Use it in your application as needed.
-const transactions = [];
+const transactions = [
+  {
+    id: 1,
+    type: 'income',
+    category: 'salary',
+    amount: 3000,
+    description: 'Monthly salary',
+    date: '2025-01-04'
+  },
+   {
+    id: 2,
+    type: 'expense',
+    category: 'rent',
+    amount: 1000,
+    description: 'Pay for the house',
+    date: '2025-01-06'
+  },
+   {
+    id: 3,
+    type: 'income',
+    category: 'Making hair',
+    amount: 5000,
+    description: 'Money earned working in the weekend',
+    date: '2025-01-29'
+  },
+   {
+    id: 4,
+    type: 'expense',
+    category: 'travel card',
+    amount: 1500,
+    description: 'Pay NS for travel card',
+    date: '2025-01-26'
+  },
+   {
+    id: 5,
+    type: 'expense',
+    category: 'tours',
+    amount: 2000,
+    description: 'Saving money for vacancy',
+    date: '2025-01-10'
+  },
+  {
+    id: 6,
+    type: 'expense',
+    category: 'Subscriptions',
+    amount: 1000,
+    description: 'Busuu,Netflix,Spotify,Data',
+    date: '2025-01-15'
+  },
+  
+  
+];
