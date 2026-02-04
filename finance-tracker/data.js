08f3b2be2998d9728323d82e0ceceb1b68a87b37
hannahwn <hannahwanjikunyongo@gmail.com>
2026-02-04 11:04:50 +0100

    adds transactions and gets total income

--- finance-tracker/data.js
+++ finance-tracker/data.js
@@ -51,3 +51,4 @@ const transactions = [
   
   
 ];
+module.exports = { transactions };
--- finance-tracker/finance.js
+++ finance-tracker/finance.js
@@ -1,9 +1,29 @@
+const { transactions } = require("./data.js");
+
 function addTransaction(transaction) {
   // TODO: Implement this function
+  let maxId = 0;
+  for (let i = 0; i < transactions.length; i++) {
+    if (transactions[i].id > maxId) {
+      maxId = transactions[i].id;
+    }
+  }
+  transaction.id = maxId + 1;
+
+  transactions.push(transaction);
+  console.log("→ Added transaction #" + transaction.id);
+
 }
 
 function getTotalIncome() {
   // TODO: Implement this function
+   let total = 0;
+  for (let i = 0; i < transactions.length; i++) {
+    if (transactions[i].type === "income") {
+      total += transactions[i].amount;
+    }
+  }
+  return total;
 }
 
 function getTotalExpenses() {
